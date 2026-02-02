0523e2840e76f75f0197362bcc7be2c2d99a48e3
helix-nuked <helix.nuked@proton.me>
2026-02-02 19:58:55 +0500

    [Optimization]

--- flake.nix
+++ flake.nix
@@ -20,27 +20,18 @@
     home-manager,
     ...
   } @ inputs: let
-    # Supported systems for your flake packages, shell, etc.
-    systems = [
-      # "aarch64-linux"
-      # i686-linux"
-      "x86_64-linux"
-      # "aarch64-darwin"
-      # "x86_64-darwin"
-    ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs systems;
+    # Supported system for your flake packages, shell, etc.
+    system = "x86_64-linux";
     # Users and Hosts
     user = "monyx";
     hostname = "laptop";
   in {
     # Your custom packages
     # Accessible through 'nix build', 'nix shell', etc
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    packages = import ./pkgs nixpkgs.legacyPackages.${system};
     # Formatter for your nix files, available through 'nix fmt'
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    formatter = nixpkgs.legacyPackages.${system}.alejandra;
 
     # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
@@ -54,7 +45,6 @@
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
-      # FIXME replace with your hostname
       ${hostname} = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
         modules = [
@@ -67,10 +57,9 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      # FIXME replace with your username@hostname
       "${user}@${hostname}" = home-manager.lib.homeManagerConfiguration {
         # Home-manager requires 'pkgs' instance
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # FIXME replace x86_64-linux with your architecure
+        pkgs = nixpkgs.legacyPackages.${system};
         extraSpecialArgs = {inherit inputs;};
         modules = [
           # > Our main home-manager configuration file <
